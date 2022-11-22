f2adee95fb4ef85475076cb2be907d60327ea91f
Yerjan <yerjan@proton.me>
2022-11-22 18:49:32 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import Welcome from "./Welcome";
-
+import Counter from "./Counter";
 function App() {
   return (
     <>
-      <Welcome name={"Yerjan"} age={24} />
-      <Welcome name={"John"} age={67} />
-      <Welcome name={"Akira"} age={15} />
-      <Welcome name={"John"} age={32} />
+      <Counter />
     </>
   );
 }
