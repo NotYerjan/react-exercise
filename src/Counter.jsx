14a85654d7b2041acc0a83cf382da6f2a9fbc752
Yerjan <yerjan@proton.me>
2022-11-22 19:03:09 +0300

    done

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
+import CounterDisplay from "./CounterDisplay";
 
 export default class Counter extends Component {
   state = {
-    count: this.props.count ?? 0,
+    count: this.props.count,
   };
   constructor(props) {
     super(props);
 
     setInterval(() => {
       this.setState((state) => {
-        return { count: state.count + (this.props.increment ?? 1) };
+        return { count: state.count + this.props.increment };
       });
-    }, this.props.interval ?? 1000);
+    }, this.props.interval);
   }
   render() {
-    return <h1>{this.state.count}</h1>;
+    return <CounterDisplay count={this.state.count} />;
   }
 }
+
+Counter.defaultProps = {
+  count: 0,
+  interval: 1000,
+  increment: 1,
+};
