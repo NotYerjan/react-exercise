2527b782dfde1851ddb78df38d3db079b0da1b13
Yerjan <yerjan@proton.me>
2022-11-22 18:18:29 +0300

    done

--- src/Age.jsx
+++ src/Age.jsx
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 
 export default class Age extends Component {
   render() {
-    return <p>Your age is {this.props.age}</p>;
+    return (
+      <>
+        {this.props.age > 18 ? (
+          <p>Your age is {this.props.age}</p>
+        ) : (
+          <p>You are very young!</p>
+        )}
+      </>
+    );
   }
 }
