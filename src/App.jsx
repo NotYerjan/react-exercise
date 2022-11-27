59dae94799f5d9b03a12ae603e7c10c886ed0119
Yerjan <yerjan@proton.me>
2022-11-27 15:32:42 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import DisplayLanguage from "./DisplayLanguage";
+import Greeting from "./Greeting";
+
 import { createContext, useState } from "react";
 
 export const LanguageContext = createContext();
 
 function App() {
   const [language, setLanguage] = useState("en");
+  const greetings = {
+    en: "Hello",
+    tr: "Merhaba",
+    ru: "Привет",
+    kz: "Сәлем",
+  };
+
   return (
     <>
-      <LanguageContext.Provider value={{ language, setLanguage }}>
+      <LanguageContext.Provider value={{ language, setLanguage, greetings }}>
         <DisplayLanguage />
+        <Greeting />
       </LanguageContext.Provider>
     </>
   );
