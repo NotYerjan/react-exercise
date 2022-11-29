f99f4277e0bd35ad15b0fd3597ce3306167df755
Yerjan <yerjan@proton.me>
2022-11-29 16:30:13 +0300

    done

--- src/ClickCounter.jsx
+++ src/ClickCounter.jsx
@@ -1,26 +1,28 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export default class ClickCounter extends Component {
-  state = {
-    count: this.props.count,
-  };
-  incrementCount = () => {
-    this.setState((state) => {
-      return { count: state.count + 1 };
-    });
-  };
+export default function ClickCounter() {
+	const [count, setCount] = useState(0);
 
-  render() {
-    return (
-      <>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.incrementCount}>+</button>
-      </>
-    );
-  }
+	const incrementCount = () => {
+		setCount((currentCount) => currentCount + 1);
+	};
+
+	return (
+		<>
+			<div>count: {count}</div>
+			<button onClick={incrementCount}>+</button>
+		</>
+	);
 }
 
-ClickCounter.defaultProps = {
-  count: 0,
-  increment: 1,
-};
+// export default class ClickCounter extends Component {
+//   state = {
+//     count: this.props.count,
+//   };
+
+//   render() {
+//     return (
+//
+//     );
+//   }
+// }
