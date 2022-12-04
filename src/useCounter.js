32cda7730b2b8d8d44a07acd3bab03d0cb0b3ed1
Yerjan <yerjan@proton.me>
2022-12-04 23:26:37 +0300

    done

--- src/useCounter.js
+++ src/useCounter.js
@@ -1,19 +1,20 @@
+import { useCallback } from "react";
 import { useState } from "react";
 
 export function useCounter(initialValue) {
 	const [count, setCount] = useState(initialValue);
 
-	const onIncrement = () => {
+	const onIncrement = useCallback(() => {
 		setCount((curent) => curent + 1);
-	};
+	}, []);
 
-	const onDecrement = () => {
+	const onDecrement = useCallback(() => {
 		setCount((curent) => curent - 1);
-	};
+	}, []);
 
-	const onReset = () => {
+	const onReset = useCallback(() => {
 		setCount(0);
-	};
+	}, []);
 
 	return { count, onIncrement, onDecrement, onReset };
 }
