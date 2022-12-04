21cbd52b5c288df607f6947c34b1aeddbfa88257
Yerjan <yerjan@proton.me>
2022-12-04 23:09:17 +0300

    done

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useGithubUser } from "./useGithubUser";
 
 export default function GithubUser({ username }) {
-	const { data, error, isLoading } = useGithubUser(username);
+	const { data, error, isLoading, onRefresh } = useGithubUser(username);
 
 	return (
 		<div>
 			{isLoading && <h3>Loading...</h3>}
 			{error && <h3>Somethinf went wrong</h3>}
 			{data && <h3>{data.name}</h3>}
+			<button onClick={() => onRefresh()}>Refresh</button>
 		</div>
 	);
 }
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -2,10 +2,14 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export function useGithubUser(username) {
-	const { data, error } = useSWR(
+	const { data, error, mutate } = useSWR(
 		username ? `https://api.github.com/users/${username}` : null,
 		fetcher,
 	);
 
-	return { data, error, isLoading: !(data || error) };
+	const onRefresh = () => {
+		mutate();
+	};
+
+	return { data, error, isLoading: !(data || error), onRefresh };
 }
