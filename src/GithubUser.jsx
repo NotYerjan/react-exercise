f7a70520df13add9cc7a742bd3a87cf17c5ed85f
Yerjan <yerjan@proton.me>
2022-12-04 19:35:25 +0300

    done

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -1,15 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useGithubUser } from "./useGithubUser";
 
 export default function GithubUser({ username }) {
-	const [data, setData] = useState(null);
-
-	useEffect(() => {
-		fetch(`https://api.github.com/users/${username}`)
-			.then((response) => response.json())
-			.then((json) => {
-				setData(json);
-			});
-	}, [username]);
-
-	return <div>{data && data.name}</div>;
+	const { data } = useGithubUser(username);
+	return <div>{data?.name}</div>;
 }
